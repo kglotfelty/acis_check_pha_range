480ed7bd321fda14c5eaec4fd992a4bd418b8983
Ken Glotfelty <kglotfelty@cfa.harvard.edu>
2020-01-14 07:50:12 -0500

    cleanup setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,8 @@
 
 
 import os
-import sys
 
 assert "ASCDS_INSTALL" in os.environ, "Please setup for CIAO before installing"
-
-# Set PYVER env variable so that it'll get replaced in setup.cfg
-ver = sys.version_info
-os.environ["PYVER"] = "python{}.{}".format(ver[0],ver[1]) 
 
 
 from distutils.core import setup
